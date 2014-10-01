66243f0605cd07212946df6750cd61e54bb2f169
juanv82@yahoo.com <juanv82@yahoo.com>
2014-09-30 22:19:47 -0500

    Prueba velocidad constante

--- javascripts/orbit.js
+++ javascripts/orbit.js
@@ -418,7 +418,8 @@
     var mu, rsoi, v;
     mu = body.gravitationalParameter;
     rsoi = body.sphereOfInfluence;
-    return v = numeric.norm2(vsoi);
+    v = numeric.norm2(vsoi);
+    return 100;
   };
 
   ejectionAngle = function(vsoi, theta, prograde) {
